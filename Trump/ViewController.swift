d94ff9529edd1ac3c5c22f08eee05196580abe25
Addison Riddleberger <addisonriddleberger@Addisons-MacBook-Pro.local>
2017-05-21 20:16:12 -0400

    Initial Commit

---
+++ Trump/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trump
+//
+//  Created by Addison Riddleberger on 5/21/17.
+//  Copyright © 2017 Addison Riddleberger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
