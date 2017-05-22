eeb8a598ae5bf99754b8d0166b557ef2c36ed32d
Addison Riddleberger <addisonriddleberger@Addisons-MacBook-Pro.local>
2017-05-22 00:04:15 -0400

    First commit

--- Trump/ViewController.swift
+++ Trump/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Thelabel: UILabel!
+    
+    var tapcount = 0
+    
+    @IBAction func PressedButton(_ sender: Any) {
+        tapcount = tapcount + 1
+        print(tapcount)
+        
+        if tapcount >= 10 {
+            Thelabel.text = "You've tapped 10 times homie"
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
